2e9e5f6b91c190fba1f096f20642391f1bb2da96
susch19 <suschi@ist-einmalig.de>
2025-01-19 16:45:07 +0100

    Initial Release

---
+++ iGPSportActivityDownloader/ActivityList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iGPSportActivityDownloader;
+
+public class ActivityList
+{
+    public int total { get; set; }
+    public Activity[] item { get; set; }
+}
+
+public class Activity
+{
+    public int RideId { get; set; }
+    public int MemberId { get; set; }
+    public string MemberName { get; set; }
+    public string MemberImg { get; set; }
+    public string Title { get; set; }
+    public string StartTime { get; set; }
+    public string ThumbPath { get; set; }
+    public string RideDistance { get; set; }
+    public string TotalAscent { get; set; }
+    public string RecordTime { get; set; }
+    public int Praise { get; set; }
+    public int IsMy { get; set; }
+    public string RideTag { get; set; }
+    public int Metric { get; set; }
+}
---
+++ iGPSportActivityDownloader/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iGPSportActivityDownloader;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+
+client.DefaultRequestHeaders.Add("Cookie", File.ReadAllText("cookie.txt"));
+
+int pages = int.MaxValue;
+
+for (int i = 1; i <= pages; i++)
+{
+
+    var res = await client.GetAsync($"https://i.igpsport.com/Activity/ActivityList?pageindex={i}");
+
+
+    var str = await res.Content.ReadFromJsonAsync<ActivityList>();
+    pages = (str.total + 9) / 10;
+    foreach (var activity in str.item)
+    {
+        var name = $"{activity.StartTime.Replace(':', '_')}.fit";
+        if (File.Exists(name))
+            continue;
+
+        var rideRequest = await client.GetAsync($"https://i.igpsport.com/fit/activity?type=0&rideid={activity.RideId}");
+        var bytes = await rideRequest.Content.ReadAsByteArrayAsync();
+        File.WriteAllBytes(name, bytes);
+        Console.WriteLine($"Written {name} with {bytes.Length / 1000}kb");
+    }
+}
